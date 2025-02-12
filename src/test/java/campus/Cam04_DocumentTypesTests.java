7b2058eb62f570312c46f73dd41fe5e9712329f6
Cihat Köse <cihat.kose@hotmail.com>
2025-02-12 21:00:43 +0100

    Update Cam04_DocumentTypesTests.java

--- src/test/java/campus/Cam04_DocumentTypesTests.java
+++ src/test/java/campus/Cam04_DocumentTypesTests.java
@@ -26,7 +26,7 @@ public class Cam04_DocumentTypesTests {
 
         Map<String, Object> userCredential = new HashMap<>();
         userCredential.put("username", "turkeyts");
-        userCredential.put("password", "TechnoStudy123");
+        userCredential.put("password", "Techno2025.%");
         userCredential.put("rememberMe", "true");
 
         Cookies cookies =
