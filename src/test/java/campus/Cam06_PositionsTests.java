54d8439c9e21616e83e06ef53276ad0ba52b552a
Cihat Köse <cihat.kose@hotmail.com>
2025-02-12 21:01:20 +0100

    Update Cam06_PositionsTests.java

--- src/test/java/campus/Cam06_PositionsTests.java
+++ src/test/java/campus/Cam06_PositionsTests.java
@@ -32,7 +32,7 @@ public class Cam06_PositionsTests {
 
         Map<String, String> userCredential = new HashMap<>();
         userCredential.put("username", "turkeyts");
-        userCredential.put("password", "TechnoStudy123");
+        userCredential.put("password", "Techno2025.%");
         userCredential.put("rememberMe", "true");
 
         Cookies cookies =
