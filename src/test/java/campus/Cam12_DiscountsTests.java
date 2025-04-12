bbf5ff22cae589ca27d1abab543a065c217410e6
Cihat Köse <cihat.kose@hotmail.com>
2025-04-13 01:10:55 +0200

    Migrate Cam12_DiscountsTests to BaseTest structure
    - Removed manual login and redundant request setup
    - Implemented BaseTest.requestSpecification
    - Fixed duplicate username in login payload
    - Cleaned and standardized test logic

--- src/test/java/campus/Cam12_DiscountsTests.java
+++ src/test/java/campus/Cam12_DiscountsTests.java
@@ -1,63 +1,25 @@
 package campus;
 
 import com.github.javafaker.Faker;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.http.ContentType;
-import io.restassured.http.Cookies;
-import io.restassured.specification.RequestSpecification;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 
-public class Cam12_DiscountsTests {
+public class Cam12_DiscountsTests extends BaseTest {
+
     Faker faker = new Faker();
     String discountID;
     String discountDescription;
     String discountCode;
-    RequestSpecification recSpec;
     Map<String, String> discount;
-
-    @BeforeClass
-    public void Login() {
-
-        baseURI = "https://test.mersys.io";
-
-        Map<String, String> userCredential = new HashMap<>();
-        userCredential.put("username", "turkeyts");
-        userCredential.put("username", "Campus25");
-        userCredential.put("password", "Campus.2524");
-
-        Cookies cookies =
-
-                given()
-
-                        .contentType(ContentType.JSON)
-                        .body(userCredential)
-
-                        .when()
-                        .post("/auth/login")
-
-                        .then()
-                        // .log().all()
-                        .statusCode(200)
-                        .extract().response().getDetailedCookies();
-
-        recSpec = new RequestSpecBuilder()
-                .setContentType(ContentType.JSON)
-                .addCookies(cookies)
-                .build();
-    }
 
     @Test
     public void createDiscounts() {
-
         discount = new HashMap<>();
 
         discountDescription = faker.nation().nationality() + faker.number().digits(5);
@@ -67,104 +29,78 @@ public class Cam12_DiscountsTests {
         discount.put("code", discountCode);
 
         discountID =
-
                 given()
-
-                        .spec(recSpec)
+                        .spec(requestSpecification)
                         .body(discount)
                         .log().body()
-
                         .when()
                         .post("/school-service/api/discounts")
-
                         .then()
                         .log().body()
                         .statusCode(201)
-                        .extract().path("id")
-        ;
+                        .extract().path("id");
     }
 
     @Test(dependsOnMethods = "createDiscounts")
     public void createDiscountsNegative() {
-
         discount.put("description", discountDescription);
         discount.put("code", discountCode);
 
         given()
-
-                .spec(recSpec)
+                .spec(requestSpecification)
                 .body(discount)
                 .log().body()
-
                 .when()
                 .post("/school-service/api/discounts")
-
                 .then()
                 .log().body()
                 .statusCode(400)
-                .body("message", containsString("already"))
-        ;
+                .body("message", containsString("already"));
     }
 
     @Test(dependsOnMethods = "createDiscountsNegative")
     public void updateDiscounts() {
+        discountDescription = "TechnoStudy-" + faker.number().digits(5);
 
         discount.put("id", discountID);
-
-        discountDescription = ("TechnoStudy-" + faker.number().digits(5));
         discount.put("description", discountDescription);
         discount.put("code", discountCode);
 
         given()
-
-                .spec(recSpec)
+                .spec(requestSpecification)
                 .body(discount)
-                // .log().body()
-
                 .when()
                 .put("/school-service/api/discounts")
-
                 .then()
                 .log().body()
                 .statusCode(200)
-                .body("description", equalTo(discountDescription))
-        ;
+                .body("description", equalTo(discountDescription));
     }
 
     @Test(dependsOnMethods = "updateDiscounts")
     public void deleteDiscounts() {
-
         given()
-
-                .spec(recSpec)
+                .spec(requestSpecification)
                 .pathParam("discountID", discountID)
                 .log().uri()
-
                 .when()
                 .delete("/school-service/api/discounts/{discountID}")
-
                 .then()
                 .log().body()
-                .statusCode(200)
-        ;
+                .statusCode(200);
     }
 
     @Test(dependsOnMethods = "deleteDiscounts")
     public void deleteDiscountsNegative() {
-
         given()
-
-                .spec(recSpec)
+                .spec(requestSpecification)
                 .pathParam("discountID", discountID)
                 .log().uri()
-
                 .when()
                 .delete("/school-service/api/discounts/{discountID}")
-
                 .then()
                 .log().body()
                 .statusCode(400)
-                .body("message", equalTo("Discount not found"))
-        ;
+                .body("message", equalTo("Discount not found"));
     }
 }
