97fd7c1a2ca493be2a36367364a0048a681ff0eb
Cihat Köse <cihat.kose@hotmail.com>
2025-04-13 01:06:53 +0200

    Migrate Cam08_SchoolLocationsTests to BaseTest structure
    - Removed manual login logic
    - Replaced recSpec with requestSpecification from BaseTest
    - Cleaned and refactored variable naming to camelCase
    - Ensured full compatibility with unified architecture

--- src/test/java/campus/Cam08_SchoolLocationsTests.java
+++ src/test/java/campus/Cam08_SchoolLocationsTests.java
@@ -1,173 +1,110 @@
 package campus;
 
 import com.github.javafaker.Faker;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.http.ContentType;
-import io.restassured.http.Cookies;
-import io.restassured.specification.RequestSpecification;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 
-public class Cam08_SchoolLocationsTests {
+public class Cam08_SchoolLocationsTests extends BaseTest {
 
     Faker faker = new Faker();
-    String SchoolLocationID;
-    String SchoolLocationName;
-    String SchoolLocationShortName;
-    String SchoolLocationCapacity;
+    String schoolLocationID;
+    String schoolLocationName;
+    String schoolLocationShortName;
+    String schoolLocationCapacity;
 
-    RequestSpecification recSpec;
-    Map<String, String> SchoolLocation;
+    Map<String, String> schoolLocation;
 
-    @BeforeClass
-    public void Setup() {
-
-        baseURI = "https://test.mersys.io";
-
-        Map<String, String> userCredential = new HashMap<>();
-        userCredential.put("username", "Campus25");
-        userCredential.put("password", "Campus.2524");
-        userCredential.put("rememberMe", "true");
-
-        Cookies cookies =
-                given()
-                        .contentType(ContentType.JSON)
-                        .body(userCredential)
-
-                        .when()
-                        .post("/auth/login")
-
-                        .then()
-                        //.log().all()
-                        .statusCode(200)
-                        .extract().response().getDetailedCookies();
-
-        recSpec = new RequestSpecBuilder()
-                .setContentType(ContentType.JSON)
-                .addCookies(cookies)
-                .build();
-    }
     @Test
     public void createSchoolLocation() {
+        schoolLocation = new HashMap<>();
 
-        SchoolLocation = new HashMap<>();
+        schoolLocationName = faker.name().firstName() + faker.number().digits(5);
+        schoolLocationShortName = faker.name().lastName() + faker.number().digits(3);
+        schoolLocationCapacity = faker.number().digits(5);
 
-        SchoolLocationName = faker.name().firstName() + faker.number().digits(5);
-        SchoolLocationShortName = faker.name().lastName() + faker.number().digits(3);
-        SchoolLocationCapacity = faker.number().digits(5);
+        schoolLocation.put("name", schoolLocationName);
+        schoolLocation.put("shortName", schoolLocationShortName);
+        schoolLocation.put("capacity", schoolLocationCapacity);
+        schoolLocation.put("type", "LABORATORY");
+        schoolLocation.put("school", "6390f3207a3bcb6a7ac977f9");
 
-        SchoolLocation.put("name", SchoolLocationName);
-        SchoolLocation.put("shortName", SchoolLocationShortName);
-        SchoolLocation.put("capacity", SchoolLocationCapacity);
-        SchoolLocation.put("type", "LABORATORY");
-        SchoolLocation.put("school", "6390f3207a3bcb6a7ac977f9");
-
-        SchoolLocationID =
-
+        schoolLocationID =
                 given()
-
-                        .spec(recSpec)
-                        .body(SchoolLocation)
+                        .spec(requestSpecification)
+                        .body(schoolLocation)
                         .log().body()
-
                         .when()
                         .post("/school-service/api/location")
-
                         .then()
                         .log().body()
                         .statusCode(201)
                         .extract().path("id");
 
-        System.out.println("LocationID = " + SchoolLocationID);
+        System.out.println("LocationID = " + schoolLocationID);
     }
 
     @Test(dependsOnMethods = "createSchoolLocation")
     public void createSchoolLocationNegative() {
-
-        SchoolLocation.put("name", SchoolLocationName);
-        SchoolLocation.put("shortName", SchoolLocationShortName);
-
         given()
-
-                .spec(recSpec)
-                .body(SchoolLocation)
+                .spec(requestSpecification)
+                .body(schoolLocation)
                 .log().body()
-
                 .when()
                 .post("/school-service/api/location")
-
                 .then()
                 .log().body()
                 .statusCode(400)
-                .body("message", containsString("already"))
-        ;
+                .body("message", containsString("already"));
     }
 
     @Test(dependsOnMethods = "createSchoolLocationNegative")
     public void updateSchoolLocation() {
-
-        SchoolLocation.put("id", SchoolLocationID);
-        SchoolLocationName = ("TechnoStudy" + faker.number().digits(5));
-        SchoolLocation.put("name", SchoolLocationName);
-        SchoolLocation.put("shortName", SchoolLocationShortName);
+        schoolLocation.put("id", schoolLocationID);
+        schoolLocationName = "TechnoStudy" + faker.number().digits(5);
+        schoolLocation.put("name", schoolLocationName);
+        schoolLocation.put("shortName", schoolLocationShortName);
 
         given()
-
-                .spec(recSpec)
-                .body(SchoolLocation)
-                // .log().body()
-
+                .spec(requestSpecification)
+                .body(schoolLocation)
                 .when()
                 .put("/school-service/api/location")
-
                 .then()
                 .log().body()
                 .statusCode(200)
-                .body("id", equalTo(SchoolLocationID))
-        ;
+                .body("id", equalTo(schoolLocationID));
     }
 
     @Test(dependsOnMethods = "updateSchoolLocation")
     public void deleteSchoolLocation() {
-
         given()
-
-                .spec(recSpec)
-                .pathParam("SchoolLocationID", SchoolLocationID)
+                .spec(requestSpecification)
+                .pathParam("SchoolLocationID", schoolLocationID)
                 .log().uri()
-
                 .when()
                 .delete("/school-service/api/location/{SchoolLocationID}")
-
                 .then()
                 .log().body()
-                .statusCode(200)
-        ;
+                .statusCode(200);
     }
 
     @Test(dependsOnMethods = "deleteSchoolLocation")
     public void deleteSchoolLocationNegative() {
         given()
-
-                .spec(recSpec)
-                .pathParam("SchoolLocationID", SchoolLocationID)
+                .spec(requestSpecification)
+                .pathParam("SchoolLocationID", schoolLocationID)
                 .log().uri()
-
                 .when()
                 .delete("/school-service/api/location/{SchoolLocationID}")
-
                 .then()
                 .log().body()
                 .statusCode(400)
-                .body("message", equalTo("School Location not found"))
-        ;
+                .body("message", equalTo("School Location not found"));
     }
 }
