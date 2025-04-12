8cdbe06c16b269ee6877839dc92f9e3476887a98
Cihat Köse <cihat.kose@hotmail.com>
2025-04-13 01:10:00 +0200

    Migrate Cam11_GradeLevelsTests to BaseTest structure
    - Removed manual login logic and recSpec
    - Implemented BaseTest inheritance for request setup
    - Standardized variable naming to camelCase
    - Updated method structure and consistency

--- src/test/java/campus/Cam11_GradeLevelsTests.java
+++ src/test/java/campus/Cam11_GradeLevelsTests.java
@@ -1,165 +1,106 @@
 package campus;
 
 import com.github.javafaker.Faker;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.http.ContentType;
-import io.restassured.http.Cookies;
-import io.restassured.specification.RequestSpecification;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 
-public class Cam11_GradeLevelsTests {
+public class Cam11_GradeLevelsTests extends BaseTest {
+
     Faker faker = new Faker();
-    String gradelevelID;
-    String gradelevelName;
-    String gradelevelShortName;
-    RequestSpecification recSpec;
+    String gradeLevelID;
+    String gradeLevelName;
+    String gradeLevelShortName;
     Map<String, String> gradeLevel;
-    
-    @BeforeClass
-    public void Setup() {
-        baseURI = "https://test.mersys.io";
-
-        Map<String, String> userCredential = new HashMap<>();
-        userCredential.put("username", "Campus25");
-        userCredential.put("password", "Campus.2524");
-        userCredential.put("rememberMe", "true");
-
-        Cookies cookies =
-                given()
-
-                        .contentType(ContentType.JSON)
-                        .body(userCredential)
-
-                        .when()
-                        .post("/auth/login")
-
-                        .then()
-                        // .log().all()
-                        .statusCode(200)
-                        .extract().response().getDetailedCookies();
-
-        recSpec = new RequestSpecBuilder()
-                .setContentType(ContentType.JSON)
-                .addCookies(cookies)
-                .build();
-    }
 
     @Test
     public void createGradeLevel() {
         gradeLevel = new HashMap<>();
-        gradelevelName = faker.name().firstName() + faker.number().digits(5);
-        gradelevelShortName = faker.name().lastName() + faker.number().digits(5);
+        gradeLevelName = faker.name().firstName() + faker.number().digits(5);
+        gradeLevelShortName = faker.name().lastName() + faker.number().digits(5);
 
-        gradeLevel.put("name", gradelevelName);
-        gradeLevel.put("shortName", gradelevelShortName);
+        gradeLevel.put("name", gradeLevelName);
+        gradeLevel.put("shortName", gradeLevelShortName);
 
-        gradelevelID =
-
+        gradeLevelID =
                 given()
-
-                        .spec(recSpec)
+                        .spec(requestSpecification)
                         .body(gradeLevel)
                         .log().body()
-
                         .when()
                         .post("/school-service/api/grade-levels")
-
                         .then()
                         .log().body()
                         .statusCode(201)
                         .extract().path("id");
 
-        System.out.println("levelID = " + gradelevelID);
+        System.out.println("gradeLevelID = " + gradeLevelID);
     }
-
 
     @Test(dependsOnMethods = "createGradeLevel")
     public void createGradeLevelNegative() {
-        gradeLevel.put("name", gradelevelName);
-        gradeLevel.put("shortName", gradelevelShortName);
+        gradeLevel.put("name", gradeLevelName);
+        gradeLevel.put("shortName", gradeLevelShortName);
 
         given()
-
-                .spec(recSpec)
+                .spec(requestSpecification)
                 .body(gradeLevel)
                 .log().body()
-
                 .when()
                 .post("/school-service/api/grade-levels")
-
                 .then()
                 .log().body()
                 .statusCode(400)
-                .body("message", containsString("already"))
-        ;
+                .body("message", containsString("already"));
     }
-
 
     @Test(dependsOnMethods = "createGradeLevelNegative")
     public void updateGradeLevel() {
-        gradeLevel.put("id", gradelevelID);
-        gradelevelName = ("TechnoStudy" + faker.number().digits(5));
-        gradeLevel.put("name", gradelevelName);
-        gradeLevel.put("shortName", gradelevelShortName);
+        gradeLevelName = "TechnoStudy" + faker.number().digits(5);
+        gradeLevel.put("id", gradeLevelID);
+        gradeLevel.put("name", gradeLevelName);
+        gradeLevel.put("shortName", gradeLevelShortName);
 
         given()
-
-                .spec(recSpec)
+                .spec(requestSpecification)
                 .body(gradeLevel)
-                // .log().body()
-
                 .when()
                 .put("/school-service/api/grade-levels")
-
                 .then()
                 .log().body()
                 .statusCode(200)
-                .body("id", equalTo(gradelevelID))
-        ;
-
+                .body("id", equalTo(gradeLevelID));
     }
 
     @Test(dependsOnMethods = "updateGradeLevel")
     public void deleteGradeLevel() {
         given()
-
-                .spec(recSpec)
-                .pathParam("levelID", gradelevelID)
+                .spec(requestSpecification)
+                .pathParam("levelID", gradeLevelID)
                 .log().uri()
-
                 .when()
                 .delete("/school-service/api/grade-levels/{levelID}")
-
                 .then()
                 .log().body()
-                .statusCode(200)
-        ;
+                .statusCode(200);
     }
 
     @Test(dependsOnMethods = "deleteGradeLevel")
     public void deleteGradeLevelNegative() {
         given()
-
-                .spec(recSpec)
-                .pathParam("levelID", gradelevelID)
+                .spec(requestSpecification)
+                .pathParam("levelID", gradeLevelID)
                 .log().uri()
-
                 .when()
                 .delete("/school-service/api/grade-levels/{levelID}")
-
                 .then()
                 .log().body()
                 .statusCode(400)
-                .body("message", equalTo("Grade Level not found."))
-        ;
+                .body("message", equalTo("Grade Level not found."));
     }
 }
