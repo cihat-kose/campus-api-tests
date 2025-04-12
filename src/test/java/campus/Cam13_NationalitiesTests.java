6932ff121174abfeec2d0b32f6dd958bb0349fe5
Cihat Köse <cihat.kose@hotmail.com>
2025-04-13 01:12:06 +0200

    Migrate Cam13_NationalitiesTests to BaseTest structure
    - Removed redundant login setup
    - Adopted BaseTest.requestSpecification
    - Refactored naming conventions for consistency
    - Cleaned up and standardized all test methods

--- src/test/java/campus/Cam13_NationalitiesTests.java
+++ src/test/java/campus/Cam13_NationalitiesTests.java
@@ -1,166 +1,99 @@
 package campus;
 
 import com.github.javafaker.Faker;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.http.ContentType;
-import io.restassured.http.Cookies;
-import io.restassured.specification.RequestSpecification;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 
-public class Cam13_NationalitiesTests {
+public class Cam13_NationalitiesTests extends BaseTest {
 
     Faker faker = new Faker();
-    String NationalitiesID;
-    String NationalitiesName;
-    Map<String, String> Nationalities;
-
-    RequestSpecification requestSpecification;
-
-    @BeforeClass
-    public void login() {
-
-        baseURI = "https://test.mersys.io";
-
-        Map<String, String> userCredential = new HashMap<>();
-        userCredential.put("username", "Campus25");
-        userCredential.put("password", "Campus.2524");
-        userCredential.put("rememberMe", "true");
-
-        Cookies cookies =
-
-                given()
-
-                        .contentType(ContentType.JSON)
-                        .body(userCredential)
-
-                        .when()
-                        .post("/auth/login")
-
-                        .then()
-//                       .log().all()
-                        .statusCode(200)
-                        .extract().response().getDetailedCookies();
-
-        requestSpecification = new RequestSpecBuilder()
-                .setContentType(ContentType.JSON)
-                .addCookies(cookies)
-                .build();
-    }
+    String nationalityID;
+    String nationalityName;
+    Map<String, String> nationality;
 
     @Test
-    public void createNationalities() {
+    public void createNationality() {
+        nationality = new HashMap<>();
 
-        Nationalities = new HashMap<>();
+        nationalityName = "serdar" + faker.number().digits(3);
+        nationality.put("name", nationalityName);
 
-        NationalitiesName = "serdar" + faker.number().digits(3);
-        Nationalities.put("name", NationalitiesName);
-
-
-        NationalitiesID =
-
+        nationalityID =
                 given()
-
                         .spec(requestSpecification)
-                        .body(Nationalities)
+                        .body(nationality)
                         .log().body()
-
                         .when()
                         .post("/school-service/api/nationality")
-
                         .then()
                         .log().body()
                         .statusCode(201)
-                        .extract().path("id")
-        ;
-        System.out.println("NationalitiesID = " + NationalitiesID);
+                        .extract().path("id");
+
+        System.out.println("nationalityID = " + nationalityID);
     }
 
-    @Test(dependsOnMethods = "createNationalities")
-    public void createNationalitiesNegative() {
-
-        Nationalities.put("name", NationalitiesName);
+    @Test(dependsOnMethods = "createNationality")
+    public void createNationalityNegative() {
+        nationality.put("name", nationalityName);
 
         given()
-
                 .spec(requestSpecification)
-                .body(Nationalities)
+                .body(nationality)
                 .log().body()
-
                 .when()
                 .post("/school-service/api/nationality")
-
                 .then()
                 .log().body()
                 .statusCode(400)
-                .body("message", containsString("already"))
-        ;
+                .body("message", containsString("already"));
     }
 
-    @Test(dependsOnMethods = "createNationalitiesNegative")
-    public void updateNationalities() {
-
-        NationalitiesName = "Tazekan - " + faker.number().digits(5);
-
-        Nationalities.put("id", NationalitiesID);
-        Nationalities.put("name", NationalitiesName);
+    @Test(dependsOnMethods = "createNationalityNegative")
+    public void updateNationality() {
+        nationalityName = "Tazekan-" + faker.number().digits(5);
+        nationality.put("id", nationalityID);
+        nationality.put("name", nationalityName);
 
         given()
-
                 .spec(requestSpecification)
-                .body(Nationalities)
-                // .log().body()
-
+                .body(nationality)
                 .when()
                 .put("/school-service/api/nationality")
-
-                .then()
-                .log().body() // show incoming body as log
-                .statusCode(200)
-                .body("name", equalTo(NationalitiesName))
-        ;
-    }
-
-    @Test(dependsOnMethods = "updateNationalities")
-    public void deleteNationalities() {
-
-        given()
-
-                .spec(requestSpecification)
-                .log().uri()
-
-                .when()
-                .delete("/school-service/api/nationality/" + NationalitiesID)
-
                 .then()
                 .log().body()
                 .statusCode(200)
-        ;
+                .body("name", equalTo(nationalityName));
     }
 
-    @Test(dependsOnMethods = "deleteNationalities")
-    public void deleteNationalitiesNegative() {
-
+    @Test(dependsOnMethods = "updateNationality")
+    public void deleteNationality() {
         given()
-
                 .spec(requestSpecification)
                 .log().uri()
-
                 .when()
-                .delete("/school-service/api/nationality/" + NationalitiesID)
+                .delete("/school-service/api/nationality/" + nationalityID)
+                .then()
+                .log().body()
+                .statusCode(200);
+    }
 
+    @Test(dependsOnMethods = "deleteNationality")
+    public void deleteNationalityNegative() {
+        given()
+                .spec(requestSpecification)
+                .log().uri()
+                .when()
+                .delete("/school-service/api/nationality/" + nationalityID)
                 .then()
                 .log().body()
                 .statusCode(400)
-                .body("message", equalTo("Nationality not  found"))
-        ;
+                .body("message", equalTo("Nationality not  found"));
     }
 }
